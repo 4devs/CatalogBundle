ab1387102ed3c7c17c01c3f8d7c662c864b8d184
andrey1s <andrey_simfi@list.ru>
2015-05-08 01:27:29 +0300

    add base sort catalog

--- Service/CatalogManager.php
+++ Service/CatalogManager.php
@@ -9,28 +9,60 @@ class CatalogManager
 {
     /** @var array */
     private $types = [];
+
     /** @var \Doctrine\Common\Persistence\ManagerRegistry */
     private $managerRegistry;
+
     /** @var string */
     private $class;
 
-    public function __construct(ManagerRegistry $managerRegistry, $class, array $types = [])
+    /** @var array */
+    private $baseOrder = [];
+
+    /**
+     * init.
+     *
+     * @param ManagerRegistry $managerRegistry
+     * @param string          $class
+     * @param array           $types
+     * @param array           $baseOrderBy
+     */
+    public function __construct(ManagerRegistry $managerRegistry, $class, array $types = [], array $baseOrderBy = [])
     {
         $this->types = $types;
         $this->managerRegistry = $managerRegistry;
         $this->class = $class;
+        $this->baseOrder = $baseOrderBy;
     }
 
-    public function getList($type)
+    /**
+     * get list items by type
+     *
+     * @param string     $type
+     * @param array|null $orderBy
+     * @param int|null   $limit
+     * @param int|null   $offset
+     *
+     * @return array
+     */
+    public function getList($type, array $orderBy = null, $limit = null, $offset = null)
     {
         if (!isset($this->types[$type])) {
             throw new NotFoundException(sprintf('type "%s" not found', $type));
         }
+        $orderBy = $orderBy ?: $this->baseOrder;
 
-        return $this->getRepository()->findBy(['type' => $type]);
+        return $this->getRepository()->findBy(['type' => $type], $orderBy, $limit, $offset);
 
     }
 
+    /**
+     * get Item by Id
+     *
+     * @param string $id
+     *
+     * @return \FDevs\CatalogBundle\Model\Item
+     */
     public function getItem($id)
     {
         $item = $this->getRepository()->find($id);
